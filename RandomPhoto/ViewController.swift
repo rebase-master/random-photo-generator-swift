e97fae03557a75d2401a1c47fd6d9faefdbe7205
Mansoor Khan <thisismansoorkhan@gmail.com>
2022-11-25 20:32:31 +0530

    Initial commit: First sample app -> random photo generator

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -2,16 +2,70 @@
 //  ViewController.swift
 //  RandomPhoto
 //
-//  Created by Mansoor (Personal) on 24/11/22.
+//  Created by Mansoor Khan on 24/11/22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    let colors: [UIColor] = [
+        .systemPink,
+        .systemRed,
+        .systemBlue,
+        .systemCyan,
+        .systemMint,
+        .systemTeal,
+        .systemOrange
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x:0, y: 0, width: view.frame.size.width, height: view.frame.size.height - 250)
+        imageView.center = view.center
+        view.addSubview(button)
+        getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapButton() {
+        getRandomPhoto()
+        view.backgroundColor = colors.randomElement()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(
+            x: 0,
+            y: view.frame.size.height-50-view.safeAreaInsets.bottom,
+            width: view.frame.size.width,
+            height: 50
+        )
+
+    }
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
     }
 
 
